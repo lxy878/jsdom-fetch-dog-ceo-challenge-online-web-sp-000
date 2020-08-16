7c7d99f9c9acc97dc05b396336fe503e4cf411b8
Xiaoyi Liu <lxy878@gmail.com>
2020-08-16 13:29:51 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -3,5 +3,9 @@ const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
 const breedUrl = 'https://dog.ceo/api/breeds/list/all'
 
 function getJson(url, fun){
-  fetch(imgUrl).then(res => res.json()).then(json => fun(json));
+  return fetch(imgUrl).then(res => res.json()).then(json => fun(json));
+}
+
+function
+
 }
