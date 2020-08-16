6b0137d3a6f0b8e5296460a0d086455ca5694516
Xiaoyi Liu <lxy878@gmail.com>
2020-08-16 13:21:43 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,3 +2,7 @@ console.log('%c HI', 'color: firebrick')
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
 
 fetch(imgUrl).then(res => res.json()).then(json => console.log(json));
+
+function message(json){
+  console.log(json.message)
+}
